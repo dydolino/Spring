39206f08e0016c03fdf3509edd7709ccf685a997
Aleksander Dytko <olek.dytko@gmail.com>
2018-11-25 20:40:39 +0100

    First commit

---
+++ src/test/java/pwr/olek/zpo/ZpoProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pwr.olek.zpo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZpoProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
